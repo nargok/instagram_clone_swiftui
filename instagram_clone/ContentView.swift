eef388e96f9622b1616c811ded4e617eb594c8cc
nargok <nargok@gmail.com>
2021-05-03 13:11:58 +0900

    Initial Commit

---
+++ instagram_clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  instagram_clone
+//
+//  Created by 後閑諒一 on 2021/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ instagram_clone/instagram_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_cloneApp.swift
+//  instagram_clone
+//
+//  Created by 後閑諒一 on 2021/05/03.
+//
+
+import SwiftUI
+
+@main
+struct instagram_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
